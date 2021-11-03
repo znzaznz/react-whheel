7f875c7fba1060165f06f909c255c72ae7d86ee8
xiaoznz <779740337@qq.com>
2021-11-03 12:01:38 +0800

    提交一次完整的代码

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: "production",
+    entry: './lib/index.tsx',
+    output: {
+        path: path.resolve(__dirname,'build/lib'), //这边之所以不能直接写path,是因为在每个不同的系统中写法不同
+        filename: "[name].[hash:4].js",
+        library: 'react-wheel',
+        libraryTarget: 'umd', //umd 是什么？
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+    ]
+}
