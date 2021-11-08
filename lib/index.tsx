fa93c1e70079e315cf9593d97803835c589c6e6c
xiaoznz <779740337@qq.com>
2021-11-08 12:01:21 +0800

    onClick类型完善

--- lib/index.tsx
+++ lib/index.tsx
@@ -6,7 +6,8 @@ import './styles.scss'
 ReactDOM.render(<div className={'icon_section'}>
     <Icon name={'weibo'}/>
     <Icon name={'wechat'}/>
-    <Icon name={'qq'} onClick={() => {
-        console.log(1);
+    <Icon name={'qq'} onClick={(e: React.MouseEvent<SVGElement | SVGUseElement>) => {
+        console.log(e.currentTarget);
+        console.log((e.target as SVGUseElement).href);
     }}/>
 </div>, document.querySelector('#root'))
