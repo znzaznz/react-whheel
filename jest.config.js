40afd1d30b5612c2232ae29e9727dd5a9f0d7609
xiaoznz <779740337@qq.com>
2021-11-14 15:14:38 +0800

    add jest-junit

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     verbose: true,
     clearMocks: false,
     collectCoverage: false,  //是否收集测试覆盖率
-    reporters: ["default"],
+    reporters: ["default","jest-junit"],
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
     moduleDirectories: ['node_modules'],
     globals: {
